2468d7eda95ccf2c77391bf6121ba5e4590a9d9b
Carlos Ledoux <carlosledoux@gmail.com>
2026-03-04 07:18:50 +0900

    Added a ton of minor changes, read the comments

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,27 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+
+  def update
+    # This is where we would update the dish, for a given day. 
+    @dish = Dish.find(params[:id])
+    if @dish.update(dish_params)
+      redirect_to week_day_path(@dish.day)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    # This is where we would show the dish, for a given day. But since we are using the recipe show page for dishes, we will just redirect to the recipe show page.
+    @dish = Dish.find(params[:id])
+    @recipe = @dish.recipe
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def dish_params
+    params.require(:dish).permit(:recipe_id, :amount, :category)
+  end
 end
--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,10 +1,19 @@
 class WeeksController < ApplicationController
   def show
     @week = Week.find(params[:id])
+    # This is to prevent users from accessing weeks that they don't own.
+    # We can do it this way, or we can just use the current_user method in the view.
+    # I think this is a better way for us to do it, because it's simpler. But we could also have the view display the current user's week and not rely on an id param at all, which might be more elegant.
+    if @week.user != current_user
+      redirect_to root_path, alert: "You are not authorized to access this week."
+    end
   end
 
   def create
+    # Weeks were missing an association to the user, so I added it here.
     @week = Week.new
+    @week.user = current_user
+    @week.month = Date.today.month
     @week.save
     redirect_to week_path(@week)
   end
